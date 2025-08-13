14e956f0af486bf7fc6f7949f922fae093e50263
Chuck Daniels <chuck@developmentseed.org>
2025-08-13 15:15:46 -0400

    Add Apache Arrow schemas
    Fixes #3
    Fixes #4
    Fixes #5
    Fixes #6

---
+++ Makefile
@@ -0,0 +1,48 @@
+.DEFAULT_GOAL := all
+
+CACHE_DIR ?= .cache
+COOKIE_JAR ?= $(HOME)/.edl-cookies
+
+# Arbitrary selection of (nearly) oldest granule from each GEDI collection
+GEDI_L2A_HDF5 := $(CACHE_DIR)/GEDI02_A_2019108002012_O01959_01_T03909_02_003_01_V002.h5
+GEDI_L2B_HDF5 := $(CACHE_DIR)/GEDI02_B_2019108002012_O01959_01_T03909_02_003_01_V002.h5
+GEDI_L4A_HDF5 := $(CACHE_DIR)/GEDI04_A_2019108002012_O01959_01_T03909_02_002_02_V002.h5
+GEDI_L4C_HDF5 := $(CACHE_DIR)/GEDI04_C_2019108002012_O01959_01_T03909_02_001_01_V002.h5
+
+CURL_OPTS := --netrc --location --fail --silent --cookie-jar $(COOKIE_JAR) --cookie $(COOKIE_JAR)
+LPDAAC_BASE_URL := https://data.lpdaac.earthdatacloud.nasa.gov/lp-prod-protected
+ORNLDAAC_BASE_URL := https://data.ornldaac.earthdata.nasa.gov/protected/gedi
+
+datasets := $(wildcard datasets/*.txt)
+schemas := $(datasets:datasets/%.txt=schemas/%)
+
+.PHONY: all
+all: $(schemas)
+
+schemas/GEDI_L2A: $(GEDI_L2A_HDF5) datasets/GEDI_L2A.txt scripts/generate_schema.py
+	uv run scripts/generate_schema.py --datasets $(@:schemas/%=datasets/%).txt --output $@ $<
+
+schemas/GEDI_L2B: $(GEDI_L2B_HDF5) datasets/GEDI_L2B.txt scripts/generate_schema.py
+	uv run scripts/generate_schema.py --datasets $(@:schemas/%=datasets/%).txt --output $@ $<
+
+schemas/GEDI_L4A: $(GEDI_L4A_HDF5) datasets/GEDI_L4A.txt scripts/generate_schema.py
+	uv run scripts/generate_schema.py --datasets $(@:schemas/%=datasets/%).txt --output $@ $<
+
+schemas/GEDI_L4C: $(GEDI_L4C_HDF5) datasets/GEDI_L4C.txt scripts/generate_schema.py
+	uv run scripts/generate_schema.py --datasets $(@:schemas/%=datasets/%).txt --output $@ $<
+
+$(GEDI_L2A_HDF5):
+	curl $(CURL_OPTS) --output $@ $(LPDAAC_BASE_URL)/GEDI02_A.002/$(shell basename $@ .h5)/$(shell basename $@)
+
+$(GEDI_L2B_HDF5):
+	curl $(CURL_OPTS) --output $@ $(LPDAAC_BASE_URL)/GEDI02_B.002/$(shell basename $@ .h5)/$(shell basename $@)
+
+$(GEDI_L4A_HDF5):
+	curl $(CURL_OPTS) --output $@ $(ORNLDAAC_BASE_URL)/GEDI_L4A_AGB_Density_V2_1/data/$(shell basename $@)
+
+$(GEDI_L4C_HDF5):
+	curl $(CURL_OPTS) --output $@ $(ORNLDAAC_BASE_URL)/GEDI_L4C_WSCI/data/$(shell basename $@)
+
+.PHONY: clean
+clean:
+	rm $(CACHE_DIR)/*.h5
---
+++ scripts/dump_schema.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from __future__ import annotations
+
+import argparse
+import typing as t
+from collections.abc import Sequence
+from pathlib import Path
+
+import pyarrow as pa
+
+
+class BooleanAction(argparse.Action):
+    def __init__(
+        self,
+        option_strings: Sequence[str],
+        dest: str,
+        **kwargs: t.Any,
+    ):
+        super().__init__(option_strings, dest, nargs=0, **kwargs)
+
+    def __call__(
+        self,
+        parser: argparse.ArgumentParser,
+        namespace: argparse.Namespace,
+        values: str | Sequence[t.Any] | None,
+        option_string: str | None = None,
+    ) -> None:
+        if option_string:
+            setattr(namespace, self.dest, not option_string.startswith("--no"))
+
+
+def parse_args(argv: Sequence[str]) -> argparse.Namespace:
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Dump contents of a binary PyArrow schema file in human-readable form."
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "schema_file",
+        type=Path,
+        help="Path to file containing PyArrow schema (as bytes)",
+    )
+    parser.add_argument(
+        "--truncate-metadata",
+        "--no-truncate-metadata",
+        dest="truncate_metadata",
+        action=BooleanAction,
+        default=False,
+        help="limit metadata key/value display to a single line of ~80 characters",
+    )
+    parser.add_argument(
+        "--show-field-metadata",
+        "--no-show-field-metadata",
+        dest="show_field_metadata",
+        action=BooleanAction,
+        default=True,
+        help="display field-level key/value metadata",
+    )
+    parser.add_argument(
+        "--show-schema-metadata",
+        "--no-show-schema-metadata",
+        dest="show_schema_metadata",
+        action=BooleanAction,
+        default=True,
+        help="display schema-level key/value metadata",
+    )
+
+    return parser.parse_args(argv)
+
+
+def main(
+    *,
+    schema_file: Path,
+    truncate_metadata: bool,
+    show_field_metadata: bool,
+    show_schema_metadata: bool,
+) -> None:
+    schema = pa.ipc.read_schema(pa.py_buffer(schema_file.read_bytes()))
+
+    print(
+        schema.to_string(
+            truncate_metadata=truncate_metadata,
+            show_field_metadata=show_field_metadata,
+            show_schema_metadata=show_schema_metadata,
+        )
+    )
+
+
+if __name__ == "__main__":
+    import sys
+
+    args = parse_args(sys.argv[1:])
+    main(**dict(args._get_kwargs()))
