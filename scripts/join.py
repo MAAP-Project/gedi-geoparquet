2f604d9c6c744205c6400383565d7c40d4c0255a
Chuck Daniels <chuck@developmentseed.org>
2025-10-27 20:25:44 -0400

    Set zstd as default compression for join script

--- scripts/join.py
+++ scripts/join.py
@@ -10,6 +10,8 @@ from cyclopts import App, Parameter
 
 from gedi_geoparquet import GEOPARQUET_METADATA
 
+type Compression = t.Literal["brotli", "gzip", "lz4", "snappy", "zstd"]
+
 # Use a built-in formatter by name: {"default", "plain"}
 # The "plain" formatter's line break logic is a bit off, so sticking with
 # "default" for now (see https://github.com/BrianPugh/cyclopts/issues/655).
@@ -21,6 +23,9 @@ def join(
     inputs: t.Annotated[list[Path], Parameter(consume_multiple=True)],
     output: Path,
     /,
+    *,
+    compression: Compression = "zstd",
+    compression_level: int | None = None,
 ) -> None:
     """Join multiple GEDI parquet files on shot_number.
 
@@ -30,6 +35,14 @@ def join(
         Paths to HDF5 GEDI files (L2A, L2B, L4A, or L4C) to join.
     output
         Path to write resulting .parquet file to.
+    compression
+        Which compression algorithm to use when writing to the parquet output file.
+    compression_level
+        Compression level for a codec.  If None is passed, the writer selects the
+        compression level for the compression codec in use.  The compression level
+        has a different meaning for each codec, so you have to read the documentation
+        of the codec you are using.  An exception is thrown if the compression codec
+        does not allow specifying a compression level.
     """
 
     def full_join(left: pl.LazyFrame, right: pl.LazyFrame) -> pl.LazyFrame:
@@ -49,7 +62,13 @@ def join(
     # lf.sink_parquet(output, metadata=GEOPARQUET_METADATA)
 
     table = lf.collect().to_arrow().replace_schema_metadata(GEOPARQUET_METADATA)
-    pq.write_table(table, output)
+
+    pq.write_table(
+        table,
+        output,
+        compression=compression,
+        compression_level=compression_level,
+    )
 
 
 if __name__ == "__main__":
